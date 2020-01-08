c509c5ff83eade182ab88e50aae0f1a233cc3fc4
Ole Petter <ole.orhagen@northern.tech>
2020-01-08 14:34:24 +0100

    Update the tests to handle the year 2020
    Also modernize the tests. The old stuff, like interfacing in order to handle
    errors in subtests has been removed in preference of running `t.Run` instead of
    local overrides. Also changed a lot of asserts to be more explicit.
    
    Changelog: None
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- license_test.go
+++ license_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Northern.tech AS
+// Copyright 2020 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -27,28 +27,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type mockT struct {
-	*testing.T
-}
-
-func (self *mockT) Fatal(args ...interface{}) {
-	// Need to get out of the calling function.
-	panic(self)
-}
-
-func expectFailure(t *testing.T) {
-	r := recover()
-	if r == nil {
-		t.Fatal("Expected failure, but did not encounter it")
-	}
-	switch r.(type) {
-	case *mockT:
-		break
-	default:
-		t.Fatal("Did not fail with correct type")
-	}
-}
-
 func resetKnownLicenses() {
 	known_license_files = []string{}
 }
@@ -58,39 +36,39 @@ func TestMockLicenses(t *testing.T) {
 
 	// Create whole src structure. This is just in case this is tested out-
 	// of-tree.
-	require.True(t, os.MkdirAll(hierarchy, 0755) == nil)
+	require.NoError(t, os.MkdirAll(hierarchy, 0755))
 	// Remove final component.
-	require.True(t, os.Remove(hierarchy) == nil)
+	require.NoError(t, os.Remove(hierarchy))
 	// And replace with symlink to here.
 	here, err := os.Getwd()
-	require.True(t, err == nil)
-	require.True(t, os.Symlink(here, hierarchy) == nil)
+	require.NoError(t, err)
+	require.NoError(t, os.Symlink(here, hierarchy))
 	defer os.RemoveAll("tmp")
 
 	// Update GOPATH.
 	oldGopath := os.Getenv("GOPATH")
-	require.True(t, os.Setenv("GOPATH", path.Join(here, "tmp")) == nil)
+	require.NoError(t, os.Setenv("GOPATH", path.Join(here, "tmp")))
 	defer os.Setenv("GOPATH", oldGopath)
 
-	CheckMenderCompliance(t)
+	assert.NoError(t, CheckMenderCompliance(t))
 
 	// Now try an unexpected license.
-	func() {
+	t.Run("Testing unexpected license", func(t *testing.T) {
+		t.Log("Testing unexpected license")
 		fd, err := os.Create("LICENSE.unexpected")
-		require.True(t, err == nil)
+		require.NoError(t, err)
 		fd.Close()
 		defer os.RemoveAll("LICENSE.unexpected")
-		defer expectFailure(t)
 
-		var mock mockT = mockT{t}
-		CheckMenderCompliance(&mock)
-	}()
+		assert.Error(t, CheckMenderCompliance(t))
+	})
 
 	// Now try a Godep without license.
-	func() {
-		require.True(t, os.MkdirAll("vendor/dummy-site.org/test-repo", 0755) == nil)
+	t.Run("Testing Godep without a license", func(t *testing.T) {
+		t.Log("Testing Godep without a license")
+		require.NoError(t, os.MkdirAll("vendor/dummy-site.org/test-repo", 0755))
 		fd, err := os.Create("vendor/dummy-site.org/test-repo/test.go")
-		require.True(t, err == nil)
+		require.NoError(t, err)
 		fd.Close()
 
 		// LIFO order, we want to remove vendor/dummy-site.org first,
@@ -98,20 +76,18 @@ func TestMockLicenses(t *testing.T) {
 		defer os.Remove("vendor")
 		defer os.RemoveAll("vendor/dummy-site.org")
 
-		defer expectFailure(t)
-
-		var mock mockT = mockT{t}
-		CheckMenderCompliance(&mock)
-	}()
+		assert.Error(t, CheckMenderCompliance(t))
+	})
 
 	// Now try a Godep without license, but with README.md.
-	func() {
-		require.True(t, os.MkdirAll("vendor/dummy-site.org/test-repo", 0755) == nil)
+	t.Run("Testing Godep without license, but with README.md", func(t *testing.T) {
+		t.Log("Testing Godep without license, but with README.md")
+		require.NoError(t, os.MkdirAll("vendor/dummy-site.org/test-repo", 0755))
 		fd, err := os.Create("vendor/dummy-site.org/test-repo/test.go")
-		require.True(t, err == nil)
+		require.NoError(t, err)
 		fd.Close()
 		fd, err = os.Create("vendor/dummy-site.org/test-repo/README.md")
-		require.True(t, err == nil)
+		require.NoError(t, err)
 		fd.Close()
 
 		// LIFO order, we want to remove vendor/dummy-site.org first,
@@ -119,22 +95,20 @@ func TestMockLicenses(t *testing.T) {
 		defer os.Remove("vendor")
 		defer os.RemoveAll("vendor/dummy-site.org")
 
-		defer expectFailure(t)
-
-		var mock mockT = mockT{t}
-		CheckMenderCompliance(&mock)
-	}()
+		assert.Error(t, CheckMenderCompliance(t))
+	})
 
 	// Now try a Godep with license in README.md, but no checksum.
-	func() {
-		require.True(t, os.MkdirAll("tmp/vendor/dummy-site.org/test-repo", 0755) == nil)
+	t.Run("Testing Godep with license, but no checksum", func(t *testing.T) {
+		t.Log("Testing Godep with license, but no checksum")
+		require.NoError(t, os.MkdirAll("tmp/vendor/dummy-site.org/test-repo", 0755))
 		require.NoError(t, os.Chdir("tmp"))
 		defer os.Chdir("..")
 		fd, err := os.Create("vendor/dummy-site.org/test-repo/test.go")
-		require.True(t, err == nil)
+		require.NoError(t, err)
 		fd.Close()
 		fd, err = os.Create("vendor/dummy-site.org/test-repo/README.md")
-		require.True(t, err == nil)
+		require.NoError(t, err)
 		fd.Close()
 
 		SetLicenseFileForDependency("vendor/dummy-site.org/test-repo/README.md")
@@ -142,34 +116,32 @@ func TestMockLicenses(t *testing.T) {
 
 		defer os.Remove("tmp")
 
-		defer expectFailure(t)
-
-		var mock mockT = mockT{t}
-		CheckMenderCompliance(&mock)
-	}()
+		assert.Error(t, CheckMenderCompliance(t))
+	})
 
 	// Now try a Godep with license in README.md, with checksum.
-	func() {
+	t.Run("Testing Godep with license in README.md, with checksum", func(t *testing.T) {
+		t.Log("Testing Godep with license in README.md, with checksum")
 		// We need a custom LIC_FILES_CHKSUM.sha256, so use a temp dir
 		// for this one.
-		require.True(t, os.MkdirAll("tmp/vendor/dummy-site.org/test-repo", 0755) == nil)
+		require.NoError(t, os.MkdirAll("tmp/vendor/dummy-site.org/test-repo", 0755))
 		require.NoError(t, os.Chdir("tmp"))
 		defer os.Chdir("..")
 		fd, err := os.Create("vendor/dummy-site.org/test-repo/test.go")
-		require.True(t, err == nil)
+		require.NoError(t, err)
 		fd.Close()
 		fd, err = os.Create("vendor/dummy-site.org/test-repo/README.md")
-		require.True(t, err == nil)
+		require.NoError(t, err)
 		fd.Close()
 		fd, err = os.Create("LICENSE")
-		fmt.Fprintln(fd, "Copyright 2019 Northern.tech")
-		require.True(t, err == nil)
+		fmt.Fprintln(fd, "Copyright 2020 Northern.tech")
+		require.NoError(t, err)
 		fd.Close()
 
 		fd, err = os.Create("LIC_FILES_CHKSUM.sha256")
-		fmt.Fprintln(fd, "4412435dcd54a30de719b5d4d1cd6e7df785d3740fffe010f4a169d82fe95e7f  LICENSE")
+		fmt.Fprintln(fd, "d8c317e825d10807ce0a5e199300a68ea5efecce74c26e92cd3472c724b73d78  LICENSE")
 		fmt.Fprintln(fd, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  vendor/dummy-site.org/test-repo/README.md")
-		require.True(t, err == nil)
+		require.NoError(t, err)
 		fd.Close()
 
 		SetLicenseFileForDependency("vendor/dummy-site.org/test-repo/README.md")
@@ -177,37 +149,35 @@ func TestMockLicenses(t *testing.T) {
 
 		defer os.Remove("tmp")
 
-		CheckMenderCompliance(t)
-	}()
+		assert.NoError(t, CheckMenderCompliance(t))
+	})
 
 	// Now try an invalid GOPATH.
-	func() {
-		require.True(t, os.Setenv("GOPATH", "/invalid") == nil)
-		defer expectFailure(t)
+	t.Run("Testing with an invalid GOPATH", func(t *testing.T) {
+		t.Log("Testing with an invalid GOPATH")
+		require.NoError(t, os.Setenv("GOPATH", "/invalid"))
 
-		var mock mockT = mockT{t}
-		CheckMenderCompliance(&mock)
-	}()
+		assert.Error(t, CheckMenderCompliance(t))
+	})
 
 	// Now try an unset GOPATH.
-	func() {
-		require.True(t, os.Unsetenv("GOPATH") == nil)
-		defer expectFailure(t)
+	t.Run("Try to unset the GOPATH", func(t *testing.T) {
+		t.Log("Try to unset the GOPATH")
+		require.NoError(t, os.Unsetenv("GOPATH"))
 
-		var mock mockT = mockT{t}
-		CheckMenderCompliance(&mock)
-	}()
+		assert.Error(t, CheckMenderCompliance(t))
+	})
 }
 
 func TestLicenses(t *testing.T) {
-	CheckMenderCompliance(t)
+	assert.NoError(t, CheckMenderCompliance(t))
 }
 
 func TestLicensesWithEnterprise(t *testing.T) {
 	// Should produce the same result as nothing.
 	SetFirstEnterpriseCommit("HEAD")
 	defer SetFirstEnterpriseCommit("")
-	CheckMenderCompliance(t)
+	assert.NoError(t, CheckMenderCompliance(t))
 }
 
 func TestCommercialLicense(t *testing.T) {
