8f3473c0a20206723d18143490e5291752ee031a
Ole Petter <ole.orhagen@northern.tech>
2022-05-16 15:55:30 +0200

    fix(commitlint): missing sign-off-line token type in the lexer
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- commitlint/testcommitlint.sh
+++ commitlint/testcommitlint.sh
@@ -369,4 +369,14 @@ Ticket: MEN-1234
 Signed-off-by: Krzysztof Jaskiewicz <krzysztof.jaskiewicz@northern.tech>
 "
 
+assert "true" \
+       "chore with cherry-pick" \
+       "chore(vendor): Add github.com/alfrunes/filelock to the vendor tree
+
+Changelog: None
+
+Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>
+(cherry picked from commit d50c065c477448bf89ac747d2607ca7497f9f8e4)"
+
+
 exit 0
