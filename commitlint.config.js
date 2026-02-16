22572a11ffb6827b20d6f1534562409966ec02d0
Paweł Poławski <pawel.polawski@northern.tech>
2026-02-17 00:40:07 +0100

    chore: Update commitlint settings
    When commit is being cherrypicked using common "git cherry-pick -x"
    command the endline of commit message contains the sentence:
    "(cherry picked from commit ... )
    
    At the same time liner expected commit message to end with
    "Signed-off-by" sequence.
    
    To satisfy those two requirements 2 custom rules has been created:
    - Require "Signed-off-by" to presented anywhere in the commit
    - If "cherry picked from" is presented - need to be at the end
    
    From some reason built in rule for signed-off-by was not working
    correct when cherry pick was also presented.
    
    Ticket: QA-1326
    Signed-off-by: Paweł Poławski <pawel.polawski@northern.tech>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,18 @@
+const CHERRY_PICK_REGEX =
+  /^\(cherry picked from commit [0-9a-f]{7,40}\)$/;
+
+const SIGNED_OFF_REGEX =
+  /^Signed-off-by:\s.+\s<[^<>]+>$/;
+
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
 
   rules: {
     'body-max-line-length': [1, 'always', 100],
     'subject-case': [1, 'always', ['lower-case', 'sentence-case']],
-    'signed-off-by': [2, 'always', 'Signed-off-by'],
+    'signed-off-anywhere': [2, 'always'],
+    'cherry-pick-at-end': [2, 'always'],
     'body-leading-blank': [2, 'always'], // body must be preceded by a blank line
                                          // exit with error if not
   },
@@ -17,5 +25,62 @@ module.exports = {
       [optional body]
   * To bypass pre-commit hooks run 'git commit --no-verify'
   >>> Use "npm run commit" for interactive prompt. <<<
-  `
+  `,
+
+  plugins: [
+    {
+      rules: {
+        'signed-off-anywhere': (parsed) => {
+          const { raw } = parsed;
+
+          if (!raw) {
+            return [true];
+          }
+
+          const lines = raw.trim().split('\n');
+          const foundIndex = lines.findIndex(line =>
+            SIGNED_OFF_REGEX.test(line.trim())
+          );
+
+          // If signed off is missing - fail
+          if (foundIndex === -1) {
+            return [
+              false,
+              'Signed-off-by: is missing in the commit message',
+            ];
+          }
+
+          return [true];
+        },
+        'cherry-pick-at-end': (parsed) => {
+          const { raw } = parsed;
+
+          if (!raw) {
+            return [true];
+          }
+
+          const lines = raw.trim().split('\n');
+          const foundIndex = lines.findIndex(line =>
+            CHERRY_PICK_REGEX.test(line.trim())
+          );
+
+          // If cherry pick is missing - pass
+          if (foundIndex === -1) {
+            return [true];
+          }
+
+          // Cherry pick line must be at the end as a consequence
+          // of "git cherry-pick -x"
+          if (foundIndex !== lines.length - 1) {
+            return [
+              false,
+              'Cherry-pick line must be the last one in the commit message',
+            ];
+          }
+
+          return [true];
+        },
+      },
+    },
+  ],
 };
